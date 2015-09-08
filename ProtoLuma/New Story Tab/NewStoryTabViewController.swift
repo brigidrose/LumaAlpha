6e95d14c771983f8f6089551e2813d3df29e50f4
Chun-Wei Chen <Eddie@Chun-Weis-MacBook-Pro.local>
2015-09-07 17:39:18 -0700

    added image size reduction logic to sendButtonTapped function before image upload to Parse.

--- ProtoLuma/New Story Tab/NewStoryTabViewController.swift
+++ ProtoLuma/New Story Tab/NewStoryTabViewController.swift
@@ -107,7 +107,8 @@ class NewStoryTabViewController: UITableViewController, UITextFieldDelegate, UIT
             }
             manager.requestImageForAsset(mediaAsset, targetSize: CGSizeMake(1200, 1200), contentMode: PHImageContentMode.Default, options: options, resultHandler:{(image:UIImage?, info:[NSObject:AnyObject]?) -> Void in
                 let storyUnit = PFObject(className: "Story_Unit")
-                storyUnit["file"] = PFFile(data: UIImagePNGRepresentation(image!)!)
+                let sizedImage = self.RBResizeImage(image!, targetSize: CGSizeMake(1200, 1200))
+                storyUnit["file"] = PFFile(data: UIImagePNGRepresentation(sizedImage)!)
                 storyUnit["description"] = self.mediaDescriptions[self.mediaAssets.indexOf(mediaAsset)!]
                 storyUnit.saveInBackgroundWithBlock({(success, error) -> Void in
                     if (error == nil){
@@ -572,4 +573,33 @@ class NewStoryTabViewController: UITableViewController, UITextFieldDelegate, UIT
             return false
         }
     }
+    
+    // Code by @hcatlin
+    func RBResizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / image.size.width
+        let heightRatio = targetSize.height / image.size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSizeMake(size.width * heightRatio, size.height * heightRatio)
+        } else {
+            newSize = CGSizeMake(size.width * widthRatio,  size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRectMake(0, 0, newSize.width, newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.drawInRect(rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    // end code by @hcatlin
+
 }
