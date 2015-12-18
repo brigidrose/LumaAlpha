388771401e23ec046ac367179e31926990bbd62b
Chris Cassano <glitch0@gmail.com>
2015-12-17 17:17:11 -0800

    Closer to Collection page looking right.  Small facebook photos of users attached to a charm

--- ProtoLuma/CharmCollectionTableViewController.swift
+++ ProtoLuma/CharmCollectionTableViewController.swift
@@ -8,14 +8,72 @@
 
 import UIKit
 
+extension UIImage {
+    var rounded: UIImage {
+        let imageView = UIImageView(image: self)
+        imageView.layer.cornerRadius = size.height < size.width ? size.height/2 : size.width/2
+        imageView.layer.masksToBounds = true
+        UIGraphicsBeginImageContext(imageView.bounds.size)
+        imageView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return result
+    }
+    var circle: UIImage {
+        let square = CGSize(width: 39, height: 39)//size.width < size.height ? CGSize(width: size.width, height: size.width) : CGSize(width: size.height, height: size.height)
+        let imageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: square))
+        imageView.contentMode = UIViewContentMode.ScaleAspectFill
+        imageView.image = self
+        imageView.layer.cornerRadius = square.width/2
+        imageView.layer.masksToBounds = true
+        UIGraphicsBeginImageContext(imageView.bounds.size)
+        imageView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return result
+    }
+}
+
+class CharmTableViewCell : UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var charmIconImage: UIImageView!
+    var addedProfileImages = Set<String>()
+    @IBOutlet var profileImages: UIStackView!
+    
+    func loadItem(title title: String, charmIconImage: UIImage, profilePhotos: Dictionary<String, UIImage>) {
+        self.charmIconImage.image = charmIconImage
+        titleLabel.text = title
+        
+        //add user photos to stackview
+        for (fbId, userImage) in profilePhotos {
+            if !addedProfileImages.contains(fbId){
+                let imageView = UIImageView(image: userImage)
+                imageView.contentMode = .ScaleAspectFit
+                profileImages.addArrangedSubview(imageView)
+                print("added profile photo to charm \(title) for user \(fbId)")
+                addedProfileImages.insert(fbId)
+            }
+        }
+        UIView.animateWithDuration(0.25) { () -> Void in
+            self.profileImages.layoutIfNeeded()
+        }
+        
+    }
+}
+
 class CharmCollectionTableViewController: UITableViewController{
     
     var userInfo:AnyObject!
     var bracelet:MBLMetaWear!
     var charms:[PFObject] = []
+    var relatedUsersOfCharms = [String: Set<String>]()
+    var profileImages = [String: UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let nib = UINib(nibName: "CharmTableViewCell", bundle: nil)
+        tableView.registerNib(nib, forCellReuseIdentifier: "CharmCell")
         
         self.tabBarController?.tabBar.items![1].enabled = false
         self.tabBarController?.tabBar.items![2].enabled = false
@@ -56,20 +114,23 @@ class CharmCollectionTableViewController: UITableViewController{
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("CharmTableViewCell", forIndexPath: indexPath)
-        cell.textLabel?.text = self.charms[indexPath.row]["name"] as? String
-        let facebookId = self.charms[indexPath.row]["gifter"]["facebookId"]
-        cell.detailTextLabel?.text = "Gifted by \(facebookId)"
-        let imgURL: NSURL = NSURL(string: "https://graph.facebook.com/\(facebookId)/picture?type=large")!
-        let request: NSURLRequest = NSURLRequest(URL: imgURL)
-        NSURLConnection.sendAsynchronousRequest(
-            request, queue: NSOperationQueue.mainQueue(),
-            completionHandler: {(response: NSURLResponse?, data: NSData?, error: NSError?) -> Void in
-                if error == nil {
-                    cell.imageView?.image = UIImage(data: data!)
-                    tableView.reloadData()
+        let cell = tableView.dequeueReusableCellWithIdentifier("CharmCell") as! CharmTableViewCell
+        let charmName = self.charms[indexPath.row]["name"] as! String
+        
+        //load profile photos
+        var profilePhotos = Dictionary<String, UIImage>()
+        let charmId = self.charms[indexPath.row].objectId!
+        if relatedUsersOfCharms.keys.contains(charmId){
+            for user in relatedUsersOfCharms[charmId]!{
+                if(self.profileImages.keys.contains(user)){
+                    profilePhotos[user] = self.profileImages[user]!
                 }
-        })
+            }
+            
+        }
+        
+        cell.loadItem(title: charmName, charmIconImage: UIImage(named: "CharmsBarButtonIcon")!, profilePhotos: profilePhotos)
+        
         return cell
     }
     
@@ -82,7 +143,7 @@ class CharmCollectionTableViewController: UITableViewController{
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 60
+        return 65
     }
     
     
@@ -92,8 +153,8 @@ class CharmCollectionTableViewController: UITableViewController{
         let queryForCharmsGifted = PFQuery(className: "Charm")
         queryForCharmsGifted.whereKey("gifter", equalTo: PFUser.currentUser()!)
         let queryForCharms = PFQuery.orQueryWithSubqueries([queryForCharmsOwned, queryForCharmsGifted])
-        //        queryForCharms.includeKey("owners")
         queryForCharms.includeKey("gifter")
+        
         queryForCharms.findObjectsInBackgroundWithBlock({(objects, error) -> Void in
             self.charms = objects!
             self.tableView.reloadData()
@@ -109,8 +170,89 @@ class CharmCollectionTableViewController: UITableViewController{
                 
                 self.tabBarController?.tabBar.items![1].enabled = true
                 self.tabBarController?.tabBar.items![2].enabled = true
+                
+                //load all profile photos in the background
+                self.loadProfilePhotos()
             }
         })
+        
+    }
+    
+    //borrowed from here: https://gist.github.com/licvido/55d12a8eb76a8103c753
+    func RBSquareImage(image: UIImage) -> UIImage {
+        let originalWidth  = image.size.width
+        let originalHeight = image.size.height
+        var x: CGFloat = 0.0
+        var y: CGFloat = 0.0
+        var edge: CGFloat = 0.0
+        
+        if (originalWidth > originalHeight) {
+            // landscape
+            edge = originalHeight
+            x = (originalWidth - edge) / 2.0
+            y = 0.0
+            
+        } else if (originalHeight > originalWidth) {
+            // portrait
+            edge = originalWidth
+            x = 0.0
+            y = (originalHeight - originalWidth) / 2.0
+        } else {
+            // square
+            edge = originalWidth
+        }
+        
+        let cropSquare = CGRectMake(x, y, edge, edge)
+        let imageRef = CGImageCreateWithImageInRect(image.CGImage, cropSquare);
+        
+        return UIImage(CGImage: imageRef!, scale: UIScreen.mainScreen().scale, orientation: image.imageOrientation)
+    }
+
+    
+
+    
+    func downloadAndSetProfilePhotos(fbIds: Set<String>){
+        print(fbIds)
+        var photosDownloaded = 0
+        for id in fbIds{
+            let imgURL: NSURL = NSURL(string: "https://graph.facebook.com/\(id)/picture?type=large")!
+            let request: NSURLRequest = NSURLRequest(URL: imgURL)
+            NSURLConnection.sendAsynchronousRequest(
+                request, queue: NSOperationQueue.mainQueue(),
+                completionHandler: {(response: NSURLResponse?, data: NSData?, error: NSError?) -> Void in
+                    if error == nil {
+                        self.profileImages[id] = self.RBSquareImage(UIImage(data: data!)!).circle
+                        photosDownloaded++
+                        if(photosDownloaded == fbIds.count){
+                            self.tableView.reloadData()
+                        }
+                    }else{
+                        print(error)
+                    }
+            })
+        }
+    }
+    
+    func loadProfilePhotos(){
+        var uniqueFacebookIds = Set<String>()
+        var retrievedCount = 0
+        for charm in self.charms{
+            var idsForCharm = Set<String>()
+            uniqueFacebookIds.insert(charm["gifter"]["facebookId"] as! String)
+            idsForCharm.insert(charm["gifter"]["facebookId"] as! String)
+            let ownersQuery = charm.relationForKey("owners").query()!
+            ownersQuery.findObjectsInBackgroundWithBlock({ (owners, error) -> Void in
+                for user in owners!{
+                    uniqueFacebookIds.insert(user["facebookId"] as! String)
+                    idsForCharm.insert(user["facebookId"] as! String)
+                }
+                self.relatedUsersOfCharms[charm.objectId!] = idsForCharm
+                retrievedCount++
+                if(retrievedCount == self.charms.count){
+                    self.downloadAndSetProfilePhotos(uniqueFacebookIds)
+                }
+            })
+        }
         
     }
 
